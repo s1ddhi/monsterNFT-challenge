62fb9c468eecfde2c08420fcc4d470440147e178
Siddhi Aphiphunya <siddhi@hey.com>
2023-12-12 18:18:29 +0000

    feat: ERC20 total supply restriction to owner

--- contracts/MTK.sol
+++ contracts/MTK.sol
@@ -5,7 +5,15 @@ contract MTK {
     string public name = "Monster Token";
     string public symbol = "MTK";
     address public owner;
+    uint256 public totalSupply = 1000000000;
+    mapping(address => uint256) balances;
+
     constructor() {
         owner = msg.sender;
+        balances[owner] = totalSupply;
+    }
+
+    function balanceOf(address account) public view returns (uint256) {
+        return balances[account];
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -5,10 +5,10 @@ const {
 
 describe("Token", () => {
     async function deploy() {
-        const [owner] = await ethers.getSigners();
+        const [owner, addr1] = await ethers.getSigners();
         const token = await ethers.deployContract("MTK");
         await token.waitForDeployment();
-        return { token, owner };
+        return { token, owner, addr1 };
     }
     describe("Deploy", () => {
         it("Should set the right owner", async () => {
@@ -24,6 +24,17 @@ describe("Token", () => {
         it("Should have the symbol MTK", async () => {
             const { token } = await deploy();
             expect(await token.symbol()).to.equal("MTK");
+        });
+
+        it("Should have a total supply of 1,000,000,000" , async () => {
+            const { token } = await deploy();
+            expect(await token.totalSupply()).to.equal(1000000000);
+        });
+
+        it("Should mint 1,000,000,000 tokens to the owner", async () => {
+            const { token, owner, addr1 } = await deploy();
+            expect(await token.balanceOf(owner.address)).to.equal(1000000000);
+            expect(await token.balanceOf(addr1.address)).to.equal(0);
         });
     })
 })
